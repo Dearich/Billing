1a52b7889345665dd5429f716e9b7e57ab219f06
dzodzievTemirlan <uccm.timmy@gmail.com>
2020-06-29 13:01:30 +0300

    откатили изменения ветки Dzodziev-Temirlan и добавили нужные изменения

--- Billing/Billing/Services/NetworkServices/GetClass.swift
+++ Billing/Billing/Services/NetworkServices/GetClass.swift
@@ -7,12 +7,37 @@
 //
 
 import Foundation
-enum Request {
-    case Get
-    case Post
-    case Delete
-}
+import Alamofire
 
 protocol NetworkSupportProtocol:class {
-    func chooseRequest(with request:Request, compliton: @escaping (Any) -> Void)
+    func chooseRequest(with request:Request, compliton: @escaping (Any?) -> Void)
+}
+
+class GetClass: NetworkSupportProtocol {
+    
+    func chooseRequest(with request: Request, compliton: @escaping (Any?) -> Void) {
+        let backgroundQueue = DispatchQueue(label: "azizbek.ru/backgroundTask", qos: .background, attributes: .concurrent)
+        switch request {
+        case .GetBilling:
+            AF.request(request.rawValue).responseJSON(queue: backgroundQueue) { (response) in
+                JsonHelper.shared.parse(requestType: .GetBilling, data: response) { (billings, transactions, error) in
+                    guard error == nil else { print(error!.localizedDescription); return }
+                    guard transactions == nil else {return}
+                    guard billings != nil else { return }
+                    compliton(billings)
+                }
+            }
+        case .GetTransaction:
+            AF.request(request.rawValue).responseJSON { (response) in
+                JsonHelper.shared.parse(requestType: .GetTransaction, data: response) { (billings, transactions, error) in
+                    guard error == nil else { print(error!.localizedDescription); return }
+                    guard billings == nil else {return}
+                    guard transactions != nil else { return }
+                    compliton(transactions)
+                }
+            }
+        default:
+            return
+        }
+    }
 }
